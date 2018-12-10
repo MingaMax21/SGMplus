d5585830091c1b6d29db698ecda1597e278d90c9
MingaMax21 <max.hoedel@tum.de>
2018-12-10 22:14:31 +0100

    minor optimization, help variable elimination. runtime still bad

--- mdaSGM.py
+++ mdaSGM.py
@@ -14,6 +14,10 @@ from skimage import color
 from skimage import io
 from skimage import img_as_ubyte
 from scipy import signal
+import time
+
+# Start timer
+start = time.time()
 
 imL = color.rgb2gray(io.imread("./data/mtlb_ex1/imL.png"))      # ADAPT PATH
 imR = color.rgb2gray(io.imread("./data/mtlb_ex1/imR.png"))      # ADAPT PATH
@@ -62,7 +66,7 @@ def rawCost(imL, imR, bS, dR, R):
     dIm = dIm.astype(np.int16)     
 
     # Border constraints
-    for i in dR: # == 5:    
+    for i in dR:   
         if i < 0:
             l = i
             r = 0
@@ -107,35 +111,41 @@ dIm, cIm = rawCost(imL, imR, bS, dR, R)
 
 def diReMap(d, pind, dimX, dimY, dimD):
 # parametrize line a1*y = a2*x +b, different parameters a1, a2, b for each direction
-# fo is parameter for negating signs, pointing in opposite direction    
+
+# Optimized boolean: Initialize all params,, rewrite as little as possible (still bad)
+    a1 = 1
+    a2 = 0
+    fo = 0
+    
     if d == 0:
         a1 =1
-        a2 = 0 
-        fo = 0
+        #a2 = 0 
+        #fo = 0
+        
     elif d == 1:
-        a1 = 1
+        #a1 = 1
         a2 = 1
-        fo = 0
+        #fo = 0
     elif d == 2:
         a1 = 0
-        a2 = 1
-        fo = 0
+        #a2 = 1
+        #fo = 0
     elif d == 3:
         a1 = 1
         a2 = -1
         fo = 1
     elif d == 4:
-        a1 = 1
+        #a1 = 1
         a2 = 0
-        fo = 1
+        #fo = 1
     elif d == 5:
-        a1 = 1
+        #a1 = 1
         a2 = 1
-        fo = 1
+        #fo = 1
     elif d == 6:
         a1 = 0
-        a2 = 1
-        fo = 1
+        #a2 = 1
+        #fo = 1
     else:
         a1 = 1
         a2 = -1
@@ -143,18 +153,16 @@ def diReMap(d, pind, dimX, dimY, dimD):
 
     if a1 != 0:
         x_inds = np.arange(0,dimX)                
-        y_inds = (a2*x_inds.T+pind)*a1        
+        y_inds = (a2*x_inds+pind)*a1        
         inds_in = np.where(np.logical_and(y_inds>=0, y_inds < dimY))        
-        inds_in = inds_in[0]      
                 
     else:
         y_inds = np.arange(0,dimY)                 
-        x_inds = -1*pind*a2*np.ones(y_inds.shape[0]) # off by 2        
+        x_inds = -1*pind*a2*np.ones(y_inds.shape[0])        
         inds_in = np.where(np.logical_and(x_inds>=0, x_inds < dimX))
-        inds_in = inds_in[0]        
         
-    x_inds = x_inds[inds_in]
-    y_inds = y_inds[inds_in]
+    x_inds = x_inds[inds_in[0]]
+    y_inds = y_inds[inds_in[0]]
     
     x_inds = np.kron(np.ones((dimD,1)), x_inds).ravel()
     y_inds = np.kron(np.ones((dimD,1)), y_inds).ravel()
@@ -166,28 +174,24 @@ def diReMap(d, pind, dimX, dimY, dimD):
         help1 = np.arange(0,dimD) 
         help2 = int((x_inds.shape[0]/33))
         z_inds = np.kron(np.ones((help2,1)), help1).ravel('F')     
-    
-#    Flip based on direction
+
+    # Flip based on direction
     if fo == 1:
         x_inds = np.flip(x_inds)
         y_inds = np.flip(y_inds)
     
-    x_inds = x_inds.astype(int)
-    y_inds = y_inds.astype(int)   
-    z_inds = z_inds.astype(int)
+    x_inds = x_inds.astype(np.int)
+    y_inds = y_inds.astype(np.int)   
+    z_inds = z_inds.astype(np.int)
        
-    # Merge indices to inds tuple    
+    # Merge indices to inds matrix (tuple)   
     indMat = (y_inds,x_inds,z_inds)
-    # Create dims tuple    
-    dims = (dimY, dimX, dimD)
-    inds = np.ravel_multi_index(indMat,dims,order='F') # Column-major indexing
     
-    return inds, indMat
+    return indMat
 
 def pathCost(slC, p1, p2):
 
-    nL = slC.shape[0]   # labels
-    nC = slC.shape[1]   # columns
+    nL, nC = slC.shape   # labels, columns
     
     # constant grades matrix
     xx = np.arange(0,nL)
@@ -196,61 +200,61 @@ def pathCost(slC, p1, p2):
     
     #write values into cGrad matrix 
     cGrad[abs(XX-YY) == 1] = p1
-    cGrad[abs(XX-YY) > 1] = p2
+    cGrad[abs(XX-YY) >  1] = p2
     
     #L slice matrix
     lrS = np.zeros(slC.shape)
     lrS[:,0] = slC[:,0]
 
     for c in range(1,nC):
-        # save previous slice
-        lrSlast = lrS[:,c-1]
+
         # calculate values C and M
-        C = slC[:,c]                          
-        M1 = np.kron(np.ones((nL,1)), lrSlast.T)
-        M = np.amin(M1 + cGrad, axis=1);
-        lrS[:,c] = C+M - np.amin(lrSlast)
+        C  = slC[:,c]                          
+        M1 = np.kron(np.ones((nL,1)), lrS[:,c-1])
+        M  = np.amin(M1 + cGrad, axis=1);
+        lrS[:,c] = C+M - np.amin(lrS[:,c-1])
         
     return(lrS)
 
 def costAgg(cIm, p1, p2, nP):
 # input: path slice of C (subC) , penalty terms
     dimY, dimX, dimD = cIm.shape
-    dimMax = dimX + dimY    
+    dims = (dimY,dimX,dimD)
+    dimMax = dimX + dimY
+    dMax = np.arange(0,2*dimMax)    
     lIm = np.zeros((dimY, dimX, dimD, nP))
     
     # iterate over directions
     for d in range(nP):
         lIi = np.zeros(cIm.shape)
-        
+        print("--- %s seconds ---" % (time.time() - start))
+        print("---step %s ----" % (d))
         # iterate over paths in direction
-        for p in range(2*dimMax):
-            pind = p-dimMax-1
-            inds, indMat = diReMap(d, pind, dimX, dimY, dimD)            
-            h1 = cIm[indMat]
-            h2 = int(inds.shape[0]/dimD)
-            slC = np.reshape(h1, [h2, dimD],order='F')
-
-            # If path exists:
+        for p in np.nditer(dMax):
+            pind = p-dimMax-1            
+            indMat = diReMap(d, pind, dimX, dimY, dimD)      
+            inds = np.ravel_multi_index(indMat,dims,order='F') # Column-major indexing
+            #h1 = cIm[indMat]
+            #h2 = int(inds.shape[0]/dimD)           
+            slC = np.reshape(cIm[indMat], [int(inds.shape[0]/dimD)  , dimD],order='F')
+            
+            # If path exists:            
             if np.all(slC.shape) != 0:
                 # evaluate cost
                 lrS = pathCost(slC.T, p1, p2)
                 # assign to output
-                lIi[indMat] = lrS.flatten()
+                lIi[indMat]= lrS.flatten()
                 lIm[:,:,:,d] = lIi
-                
+                         
     return lIm
 
 lIm = costAgg(cIm, p1, p2, nP)
 
-def dispMap(lIm, dR):
-    
-    S = np.sum(lIm,axis=3)
-    dMap = np.argmin(S,axis=2)+dR[0]
+# Sum across paths
+S = np.sum(lIm,axis=3)
 
-    return dMap
-
-dMap = dispMap(lIm, dR)
+# Final disparity map:
+dMap = np.argmin(S,axis=2)+dR[0]
 
 fig,axes = plt.subplots(1,1)
 axes.set_xlabel("X")
@@ -258,3 +262,5 @@ axes.set_ylabel("Y")
 axes.set_title("Disparity Image")
 axes.imshow(dMap,cmap='gray')
 plt.show()
+
+print("--- %s seconds ---" % (time.time() - start))
